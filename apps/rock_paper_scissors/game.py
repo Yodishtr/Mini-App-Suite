f113cc834ca7d01dfdbc8db536128a993ec4c69d
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-09-19 00:22:04 -0400

    designing and implementing the game.py file. Added a views.py file to be used by the app.py and it wraps common/ui/__init__.py

--- apps/rock_paper_scissors/game.py
+++ apps/rock_paper_scissors/game.py
@@ -24,9 +24,82 @@ class Move(Enum):
             return False
 
 
-"""This class represents the game logic for rock paper scissors"""
+class EventEmitter:
+
+    """
+    This class is an Event Emitter that allows the game to send payloads of data to the ui via
+    the app
+    """
+
+    def __init__(self):
+        self.my_events = {}
+
+    def on(self, event, handler):
+        """Sets up event's handler"""
+        if event not in self.my_events:
+            self.my_events[event] = []
+        self.my_events[event].append(handler)
+
+    def emit(self, event, payloadData=None):
+        """Assigned handler emits payload data to ui via handler return format"""
+        if event in self.my_events:
+            for handler in self.my_events[event]:
+                handler(payloadData)
+
 
 class Game:
+
+    """This class represents the game logic for rock paper scissors"""
     def __init__(self, difficulty: str, num_round=5):
+        """Maybe add a storage device as an argument to the game."""
         self.difficulty = difficulty
-        self.rounds = num_round
+        self.numRounds = num_round
+        self.currentRound = 0
+        self.playerScore = 0
+        self.computerScore = 0
+
+    def start(self):
+        """Can make it emit an RPS event? so that changes can be propagated to UI"""
+        self.currentRound = 0
+        self.computerScore = 0
+        self.playerScore = 0
+
+    def reset(self):
+        """resets the game"""
+        self.currentRound = 0
+        self.computerScore = 0
+        self.playerScore = 0
+
+    def legalMove(self):
+        """Returns a list of the allowed moves"""
+        return [Move.ROCK, Move.PAPER, Move.SCISSORS]
+
+    def playRound(self, playerMove: Move):
+        """Returns a small ui friendly round result (could be a dict) and also emits
+        an event to allow ui to render. app.py extracts user input and creates a Move object which
+        is passed to this method.
+        """
+        if playerMove not in self.legalMove():
+            return "Invalid input"
+        else:
+            if self.difficulty == "easy":
+                result = self.easyCompMove(playerMove)
+                return result
+            elif self.difficulty == "medium":
+                result = self.mediumCompMove(playerMove)
+                return result
+            elif self.difficulty == "hard":
+                result = self.hardCompMove(playerMove)
+                return result
+
+
+    def easyCompMove(self, playerMove):
+        preset = {1: Move.ROCK, 2: Move.PAPER, 3: Move.SCISSORS}
+
+
+    def mediumCompMove(self, playerMove):
+        pass
+
+
+    def hardCompMove(self, playerMove):
+        pass
---
+++ apps/rock_paper_scissors/views.py
@@ -0,0 +1,6 @@
+"""
+Creates the specific UI for rock paper scissors.
+Utilizes the common/ui/__init__ as a wrapper to keep fonts, colors and styles
+consistent across the app.
+
+"""
