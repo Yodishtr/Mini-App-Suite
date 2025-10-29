63c24c100ef365046080d900173af05eafe58218
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-10-29 16:38:48 -0400

    nearly done with the controller for the voice recorder

--- apps/voice_recorder/app.py
+++ apps/voice_recorder/app.py
@@ -1,4 +1,5 @@
-from PySide6.QtCore import QObject, Qt, Slot
+import numpy as np
+from PySide6.QtCore import QObject, Qt, QTimer, Slot
 from apps.voice_recorder.recorder import (AudioRecorder, AudioConfig,
                                           NoRecordingAvailable, PlayRecordingInSession,
                                           RecordingInSession)
@@ -35,6 +36,63 @@ class VoiceRecorder(QObject):
         self.audio_recorder_views.stop_button.clicked.connect(self.stop_requested)
         self.audio_recorder_views.play_button.clicked.connect(self.play_requested)
         self.audio_recorder_views.save_wav_button.clicked.connect(self.save_wav_requested)
+
+        # timer for the db level, clip level and meter bar
+        self.timer = QTimer()
+        self.timer.setInterval(75)
+        self.timer.timeout.connect(self.timer_tick)
+
+
+    def compute_byte_slice_size(self):
+        current_sample_rate = self.audio_config.rate
+        current_channels = int(self.audio_config.channels)
+        current_sample_format = self.audio_config.sample_format
+        if current_sample_format == "int16":
+            bytes_per_sample = 2
+        else:
+            bytes_per_sample = 4
+        window_ms = 150  # in milliseconds
+        frames_in_window = int(current_sample_rate * (window_ms / 1000))
+        bytes_per_frames = current_channels * bytes_per_sample
+        bytes_in_window = frames_in_window * bytes_per_frames
+
+        current_byte_frame = (self.audio_recorder_logic.
+                              get_raw_bytes(self.audio_recorder_logic.frames))
+        tail = current_byte_frame[-bytes_in_window:]
+        if len(tail) == 0:
+            return (0, 0, 0, False)
+        rem = len(tail) % bytes_per_frames
+        if rem != 0:
+            tail = tail[:-rem]
+
+        # normalization
+        if current_sample_format == "int16":
+            int_array = np.frombuffer(tail, dtype=np.int16)
+            normalized_float_array = ((int_array.astype('float32') / 32768.0)
+                                      .reshape(-1, current_channels))
+        elif current_sample_format == "int32":
+            int_array = np.frombuffer(tail, dtype=np.int32)
+            normalized_float_array = ((int_array.astype('float32') / 2147483648.0).
+                                      reshape(-1, current_channels))
+        elif current_sample_format == "float32":
+            normalized_float_array = (np.frombuffer(tail).reshape(-1, current_channels))
+
+        samples = np.mean(np.abs(normalized_float_array), axis=1)
+        rms = np.sqrt(np.mean(samples ** 2))
+        peak = np.max(np.abs(samples))
+        db = 20 * np.log10(rms + 1e-12)
+        if peak >= 0.999:
+            is_clipping = True
+        else:
+            is_clipping = False
+        return (rms, peak, db, is_clipping)
+
+    @Slot(bool)
+    def timer_tick(self):
+        """
+        does the computation for the VU, clip and db
+        """
+
 
     @Slot(bool)
     def record_requested(self):
